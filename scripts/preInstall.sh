d652f1a6e0a96cade53d791269133c1811c4ba5e
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-06 10:04:45 +0200

    update

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,10 +1,22 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
-apt install jq -y
 
 mkdir -p ./postgres_data
 mkdir -p ./redis_data
 
 chown -R 1000:1000 ./postgres_data
 chown -R 1000:1000 ./redis_data
+
+AP_POSTGRES_PASSWORD=${AP_POSTGRES_PASSWORD:-`openssl rand -hex 32`}
+AP_JWT_SECRET=${AP_JWT_SECRET:-`openssl rand -hex 32`}
+ENCRYPTION_KEY=${ENCRYPTION_KEY:-`openssl rand -hex 16`}
+AP_ENCRYPTION_KEY=${AP_ENCRYPTION_KEY:-`openssl rand -hex 16`}
+
+cat << EOT >> ./.env
+
+AP_POSTGRES_PASSWORD=${AP_POSTGRES_PASSWORD}
+AP_JWT_SECRET=${AP_JWT_SECRET}
+ENCRYPTION_KEY=${ENCRYPTION_KEY}
+AP_ENCRYPTION_KEY=${AP_ENCRYPTION_KEY}
+EOT
