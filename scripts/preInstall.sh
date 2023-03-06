7e804e19df462578747e2cdfb2a2b81aaf501b76
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-06 10:07:16 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -11,7 +11,7 @@ chown -R 1000:1000 ./redis_data
 AP_POSTGRES_PASSWORD=${AP_POSTGRES_PASSWORD:-`openssl rand -hex 32`}
 AP_JWT_SECRET=${AP_JWT_SECRET:-`openssl rand -hex 32`}
 ENCRYPTION_KEY=${ENCRYPTION_KEY:-`openssl rand -hex 16`}
-AP_ENCRYPTION_KEY=${AP_ENCRYPTION_KEY:-`openssl rand -hex 16`}
+AP_ENCRYPTION_KEY=${AP_ENCRYPTION_KEY:-`openssl rand -hex 32`}
 
 cat << EOT >> ./.env
 
